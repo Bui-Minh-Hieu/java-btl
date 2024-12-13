7a0a637bb6a9c3760ad4b40d6322c6de7e735978
Bui-Minh-Hieu <buiminhhieuit@gmail.com>
2024-12-13 23:11:13 +0700

    Cap nhat 13-12

--- btl/src/qlhp/Main.java
+++
@@ -1,7 +0,0 @@
-package qlhp;
-import javax.swing.*;
-public class Main {
-	public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
-    }
-}

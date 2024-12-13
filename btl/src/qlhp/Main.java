0e15a7a0952fe8d6d99d999be44a592700d6ec29
khanhvu254 <121953497+khanhvu254@users.noreply.github.com>
2024-12-13 21:15:00 +0700

    Hoc Phan

--- btl/src/qlhp/Main.java
+++ btl/src/qlhp/Main.java
@@ -1,7 +1,6 @@
 package qlhp;
 import javax.swing.*;
-
-public class Run {
+public class Main {
 	public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
     }
